f3573b9c22494d14633561ab4a7643eb5e087e15
AkatsukiDeki <124888750+AkatsukiDeki@users.noreply.github.com>
2023-11-12 17:46:53 +0400

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,52 @@
+import time
+
+from bs4 import BeautifulSoup
+import csv
+import re
+import requests
+for year in range(2007,2024):
+    for moths in range(1,13):
+        Url = "https://www.gismeteo.ru/diary/4618/" + str(year) + "/" + str(moths) + "/"
+        headers = {
+        "Accept":"*/*",
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0 (Edition Yx GX)"
+        }
+        req = requests.get(Url,headers = headers)
+        src = req.text
+        try:
+            with open("index.html", "w") as file:
+                file.write(src)
+        except Exception:
+            continue
+        soup = BeautifulSoup(src,"lxml")
+        try:
+            table = soup.find("table").find("tbody").find_all("tr")
+        except Exception:
+            continue
+        data_table = []
+        for item in table:
+            data_td = item.find_all('td')
+            day = data_td[0].text
+            temp_morning = data_td[1].text
+            presure_morning = data_td[2].text
+            wind_morning = data_td[5].text
+            temp_evening = data_td[6].text
+            wind_evening = data_td[10].text
+            presure_evening = data_td[7].text
+            data_table.append(
+                {
+                    "day": day +"." + str(moths)+"."+str(year),
+                    "temp_morning": temp_morning,
+                    "presure_morning": presure_morning,
+                    "wind_morning": wind_morning,
+                    "temp_evening": temp_evening,
+                    "presure_evening": presure_evening,
+                    "wind_evening": wind_evening,
+
+                }
+            )
+
+        with open('dataset.csv', 'a', newline='') as csvfile:
+            for item in data_table:
+                writer = csv.writer(csvfile,delimiter=",")
+            writer.writerow([item["day"],item["temp_morning"],item["presure_morning"],item["wind_morning"],item["temp_evening"],item["presure_evening"],item["wind_evening"]])
